5a94f60130cd5a017584c273c743adf7ee2f21ac
Thomas Cosby <cosbyts@gmail.com>
2017-08-08 13:11:35 -0700

    Created 'client' directory.  Added main index.jsx file, and beginning structure for the left and right main component blocks.
    Created webpack.config file, with default file directories.  Can re-configure for updated file structure.
    
    Added dependencies to package.json:
    
      - webpack
      - babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+
+  context: SRC_DIR,
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  }
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: [
+    path.join(__dirname, 'node_modules');
+  ]
+
+};
