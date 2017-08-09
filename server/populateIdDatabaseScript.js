3dee4d523b0f97f82a88af20310345116126f36c
Miles Sorce <m.sorce@gmail.com>
2017-08-09 11:46:46 -0700

    Start updating script to match new db schema

--- server/populateIdDatabaseScript.js
+++ server/populateIdDatabaseScript.js
@@ -38,17 +38,43 @@ foodTypes.forEach(foodType => {
   })
   .then(data => {
     data.forEach(recipe => {
-      let newRecipe = new Recipe(data);
-      newRecipe.save(err => {
-        if (err) {
-          throw err;
-        } else {
-          console.log('Data successfully saved');
-        }
-      });
+      if (recipe.rating >= 4) {
+        let newRecipe = new Recipe(data);
+        newRecipe.name = recipe.name;
+        newRecipe.
+
+        newRecipe.save(err => {
+          if (err) {
+            throw err;
+          } else {
+            console.log('Data successfully saved');
+          }
+        });
+      }
     });
   })
   .catch(err => {
     console.log(`Failed to fetch data`);
   });
 });
+
+
+var Thing = mongoose.model('Thing', schema);
+
+var m = new Thing;
+m.name = 'Statue of Liberty';
+m.age = 125;
+m.updated = new Date;
+m.binary = new Buffer(0);
+m.living = false;
+m.mixed = { any: { thing: 'i want' } };
+m.markModified('mixed');
+m._someId = new mongoose.Types.ObjectId;
+m.array.push(1);
+m.ofString.push("strings!");
+m.ofNumber.unshift(1,2,3,4);
+m.ofDates.addToSet(new Date);
+m.ofBuffer.pop();
+m.ofMixed = [1, [], 'three', { four: 5 }];
+m.nested.stuff = 'good';
+m.save(callback);
