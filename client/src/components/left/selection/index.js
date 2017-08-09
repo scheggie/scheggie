1cdd0f107d12e61df1790573e6c53d06a37d1900
Thomas Cosby <cosbyts@gmail.com>
2017-08-09 14:37:44 -0700

    (Feat) Implemented dialog successfully on Selection component.

--- client/src/components/left/selection/index.js
+++ client/src/components/left/selection/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 import Flexbox from 'flexbox-react';
 
 const general_style = {
@@ -15,8 +15,8 @@ const image_style = {
 
 const title_style = {
   textAlign: 'center',
-  padding: '0px 0 0px 0',
   backgroundColor: '#e6e6e6'
+
 };
 
 const desc_style = {
@@ -24,18 +24,78 @@ const desc_style = {
   padding: '0px 10px 10px 10px'
 };
 
-var Selection = () => (
+class Selection extends React.Component {
 
-  <Flexbox flexDirection="row" maxWidth="33vw" style={general_style}>
-    <Flexbox flexGrow="1" style={image_style}>
-      <img style={{maxWidth: '100%'}} src="http://tuvanranghammat.vn/assets/lo-mieng-nen-an-gi-e1469805715695.jpg"/>
-    </Flexbox>
-    <Flexbox flexDirection="column" flexGrow="2" style={{}}>
-      <h3 style={title_style}>Food Title</h3>
-      <p style={desc_style}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-    </Flexbox>
-  </Flexbox>
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewMore: false
+    }
+    this.viewMoreHandler = this.viewMoreHandler.bind(this);
+  }
 
-)
+  viewMoreHandler() {
+    this.setState({
+      viewMore: !this.state.viewMore
+    })
+  }
+
+  render() {
+
+    const actions = [
+      <FlatButton
+        label="Close"
+        secondary={true}
+        onTouchTap={this.viewMoreHandler}
+      />
+    ]
+
+    let dialogBox = null;
+
+    if (this.state.viewMore) {
+      dialogBox = (
+        <Dialog
+          title="INSERT_TITLE_HERE"
+          actions={actions}
+          modal={false}
+          open={this.viewMoreHandler}
+          onRequestClose={this.viewMoreHandler}
+        >
+          <div style={{textAlign: 'center'}}>
+            <img style={{maxWidth: '100%'}} src="http://tuvanranghammat.vn/assets/lo-mieng-nen-an-gi-e1469805715695.jpg"/>
+          </div>
+          <div>
+            <h3>INSERT_FOOD_ITEM_NAME_HERE</h3>
+            <p>INSERT_DESCRIPTION</p>
+            <ul>
+              <li>INGREDIENTS</li>
+              <li>INGREDIENTS</li>
+              <li>INGREDIENTS</li>
+              <li>INGREDIENTS</li>
+              <li>INGREDIENTS</li>
+            </ul>
+          </div>
+        </Dialog>
+      )
+    } else {
+      dialogBox = null;
+    }
+
+    return (
+      <Flexbox flexDirection="row" maxWidth="33vw" style={general_style}>
+        <Flexbox flexGrow="1" style={image_style}>
+          <img style={{maxWidth: '100%'}} src="http://tuvanranghammat.vn/assets/lo-mieng-nen-an-gi-e1469805715695.jpg"/>
+        </Flexbox>
+        <Flexbox flexDirection="column" flexGrow="2">
+          <h3 style={title_style}>Food Title</h3>
+          <p style={desc_style}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          <FlatButton label="Read More" onTouchTap={this.viewMoreHandler}/>
+        </Flexbox>
+        { dialogBox }
+      </Flexbox>
+    )
+  }
+
+}
 
 export default Selection;
