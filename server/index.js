c437b241bfb88ec7846bdfc6da3ee0ad04b887d0
Miles Sorce <m.sorce@gmail.com>
2017-08-10 12:04:18 -0700

    Update Yummly key/ID variables after adding Facebook ID/key to config file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ const webpack = require('webpack');
 const webpackConfig = require('../webpack.config');
 const app = express();
 const compiler = webpack(webpackConfig);
+const passport = require('passport-facebook');
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 
 
