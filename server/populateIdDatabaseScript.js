72e0e9af118ac887b88476e10babf8b5b9c06d6a
Miles Sorce <m.sorce@gmail.com>
2017-08-09 18:41:05 -0700

    (feaat) Finalize script to save IDs to DB

--- server/populateIdDatabaseScript.js
+++ server/populateIdDatabaseScript.js
@@ -1,3 +1,9 @@
+/* READ THIS BEFORE RUNNING SCRIPT:
+
+THIS FUNCTION IS FOR POPULATINZG THE DB WITH RECIPE IDs.
+ONLY RUN FOR ~8 FOOD TYPES AT A TIME. OTHERWISE YOU MAY ENCOUNTER ERRORS. */
+
+
 const rp = require('request-promise');
 const request = require('request');
 const config = require('../config.js');
@@ -38,33 +44,35 @@ const foodTypes = [
   'curry'
 ];
 
-// THIS FUNCTION IS FOR POPULATINZG THE DB WITH RECIPE IDs
-foodTypes.forEach(foodType => {
-  rp({
-    uri: `http://api.yummly.com/v1/api/recipes?_app_id=${config.APP_ID}&_app_key=${config.APP_KEY}&allowedDiet[]=387^Lacto-ovo vegetarian&q=${foodType}&requirePictures=true&maxResult=${resultLimit}`
-  })
-  .then(data => {
-    data = JSON.parse(data);
-    data = data.matches;
-    data.forEach(recipe => {
-      if (recipe.rating >= 4) {
-        let newRecipe = new Recipe;
-        newRecipe.name = recipe.id;
-        newRecipe.fullDataSorter = false;
-        newRecipe.rating = recipe.rating;
-        newRecipe.abridgedData = recipe;
-        newRecipe.fullData = null;
-        newRecipe.save(err => {
-          if (err) {
-            throw err;
-          } else {
-            console.log('Data successfully saved');
-          }
-        });
-      }
+let timeBase = 4000;
+foodTypes.forEach((foodType, index) => {
+  setTimeout(() => {
+    rp({
+      uri: `http://api.yummly.com/v1/api/recipes?_app_id=${config.APP_ID}&_app_key=${config.APP_KEY}&allowedDiet[]=387^Lacto-ovo vegetarian&q=${foodType}&requirePictures=true&maxResult=${resultLimit}`
+    })
+    .then(data => {
+      data = JSON.parse(data);
+      data = data.matches;
+      data.forEach(recipe => {
+        if (recipe.rating >= 4) {
+          let newRecipe = new db.Recipe;
+          newRecipe.name = recipe.id;
+          newRecipe.fullDataSorter = false;
+          newRecipe.rating = recipe.rating;
+          newRecipe.abridgedData = recipe;
+          newRecipe.fullData = null;
+          newRecipe.save(err => {
+            if (err) {
+              throw err;
+            } else {
+              console.log('Data successfully saved');
+            }
+          });
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(`Failed to fetch data`);
-  });
+  }, timeBase * index);
 });
