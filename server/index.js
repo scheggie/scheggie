5ac5d4edc2c2a31bfa3a7001dd2364b62e52b22f
Miles Sorce <m.sorce@gmail.com>
2017-08-10 13:47:11 -0700

    (feat) Add passport configuration, .get endpoints

--- server/index.js
+++ server/index.js
@@ -3,20 +3,30 @@ const webpack = require('webpack');
 const webpackConfig = require('../webpack.config');
 const app = express();
 const compiler = webpack(webpackConfig);
-const passport = require('passport-facebook');
+const passport = require('passport');
+const Strategy = require('passport-facebook').Strategy;
 const config = require('../config.js');
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.configure(function() {
+  app.use(express.static(__dirname + '/../client/dist'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+});
 
-passport.use(new FacebookStrategy({
+
+passport.use(new Strategy({
     clientID: config.FACEBOOK_clientID,
     clientSecret: config.FACEBOOK_clientSecret,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    // User's Facebook profile is supplied as the user record
+
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+    console.log('accessToken', accessToken, 'refreshToken', refreshToken, 'profile', profile);
+    return cb(null, profile);
   }
 ));
 
@@ -24,10 +34,18 @@ passport.use(new FacebookStrategy({
 
 
 // ************************************
+  
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 
 
-    // ALL ENDPOINTS WILL LIVE HERE
 
 
 // ************************************
