1cb71577b262b4bf3dae9eab8030a9107bd3023a
james <JMCritelli6@Yahoo.com>
2017-08-14 17:36:19 -0700

    This should have gotten rid of all merge conflicts

--- databases/users.js
+++ databases/users.js
@@ -5,7 +5,12 @@ var userSchema = mongoose.Schema({
   facebookId: String,
   name: String,
   email: String,
+<<<<<<< HEAD
   weeks: Array
+=======
+  week_one: Array,
+  week_two: Array 
+>>>>>>> Wrote out comments for adding recipes to the calendar for both front-end, server, and DB. Added routes to connect server to DB
 });
 
 var User = mongoose.model('User', userSchema);
