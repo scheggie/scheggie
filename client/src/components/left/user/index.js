78d63a3efdc81aa08ac10cfb19a120278a7d28ca
Thomas Cosby <cosbyts@gmail.com>
2017-08-15 17:10:31 -0700

    (feat) Refactor results entries into grid format.
    (feat) Refactor user component to smaller, sleeker format.
    
    (feat) Refactor selection component to more consistent structure.

--- client/src/components/left/user/index.js
+++ client/src/components/left/user/index.js
@@ -1,44 +1,59 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import Flexbox from 'flexbox-react';
-
-
-const toolbar_style = {
-  background: 'linear-gradient(to bottom, #669900 7%, #336600 81%)',
-  height: '90px',
-  borderRadius: '5px'
-}
+import FlatButton from 'material-ui/FlatButton';
 
 var User = (props) => (
 
-  <Toolbar style={toolbar_style}>
-    <ToolbarGroup>
-      <IconMenu
-          iconButtonElement={
-            <IconButton touch={true} style={{margin: '-46px 0 0 -10px'}}>
-              <Avatar size={70} src={props.user.picture.data.url} style={{boxShadow: '2px 2px 4px #1a3300'}}/>
-            </IconButton>
-          }
-        >
-        <MenuItem primaryText="Change Avatar" />
-        <MenuItem primaryText="Log Out" onTouchTap={props.logOut} />
-      </IconMenu>
-    </ToolbarGroup>
-    <ToolbarGroup>
-      <ToolbarTitle
-        text={`Hello ${props.user.name.split(' ')[0]}!`}
-        style={{color: '#ffffff'}}/>
-    </ToolbarGroup>
-  </Toolbar>
+  <div style={{
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '50px',
+    backgroundColor: 'rgb(40, 130, 150)'
+  }}>
+    <div style={{
+      paddingLeft: '15px'
+    }}>
+      <Avatar size={40} src={props.user.picture.data.url} style={{boxShadow: '2px 2px 4px #1a3300'}}/>
+    </div>  
+    <div style={{
+      paddingRight: '15px'
+    }}>
+      <FlatButton
+        label="Log Out"
+        hoverColor="#EF5350"
+        rippleColor="#FFEBEE"
+        onTouchTap={props.logOut}
+        style={{
+          color: 'white'
+        }}
+      />
+    </div>
+  </div>
 
 )
+
+// Toolbar style={toolbar_style}>
+//     <ToolbarGroup>
+//       <IconMenu
+//           iconButtonElement={
+//             <IconButton touch={true} style={{margin: '-46px 0 0 -10px'}}>
+//             </IconButton>
+//           }
+//         >
+//         <MenuItem primaryText="Change Avatar" />
+//         <MenuItem primaryText="Log Out" onTouchTap={props.logOut} />
+//       </IconMenu>
+//     </ToolbarGroup>
+//     <ToolbarGroup>
+//       <ToolbarTitle
+//         text={`Hello ${props.user.name.split(' ')[0]}!`}
+//         style={{color: '#ffffff'}}/>
+//     </ToolbarGroup>
+//   </Toolbar>
+
+
 
 export default User;
