368f222e12906f0be104c23312f9277d2ad995e4
Miles Sorce <m.sorce@gmail.com>
2017-08-17 13:39:46 -0700

    (test) fix error

--- databases/users.js
+++ databases/users.js
@@ -18,10 +18,10 @@ userSchema.statics.getUserById = function(id) {
 
 // Save recipe to user's favorites
 userSchema.statics.saveRecipeToFavorites = function(user, selectedRecipe) {
-  if (!user.favorites[selectedRecipe.name]) {
+  if (!user.favRecipes[selectedRecipe.name]) {
     Recipe.getFullRecipeByName(selectedRecipe.name).
       then(recipe => {
-        user.favorites[selectedRecipe.name] = JSON.parse(recipe);
+        user.favRecipes[selectedRecipe.name] = JSON.parse(recipe);
         user.save(err => {
           if (err) {
             throw err;
@@ -33,8 +33,8 @@ userSchema.statics.saveRecipeToFavorites = function(user, selectedRecipe) {
 
 // Remove recipe from user's favorites
 userSchema.statics.removeRecipeFromFavorites = function(user, selectedRecipe) {
-  if (user.favorites[selectedRecipe.name]) {
-    delete user.favorites[selectedRecipe.name];
+  if (user.favRecipes[selectedRecipe.name]) {
+    delete user.favRecipes[selectedRecipe.name];
     user.save(err => {
       if (err) {
         throw err;
