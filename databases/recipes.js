7e9ffa6833eb7807cee1c5b7804dab85a9dc68d1
Miles Sorce <m.sorce@gmail.com>
2017-08-16 17:42:54 -0700

    (clean) Fix error

--- databases/recipes.js
+++ databases/recipes.js
@@ -13,6 +13,13 @@ recipeSchema.statics.getFullRecipeByName = function(name) {
   return this.find({'name': name});
 };
 
+// Get 20 recipes with search query in name
+recipeSchema.statics.getFullRecipesForSearchResults = function(query) {
+  return this.find({'name': {$regex : '.*${req.body.query}.*'}}).
+    limit(20);
+};
+
+
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
