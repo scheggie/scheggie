263b4a33aa7c8eca4930e50fef1efb0fa48eb0f0
Miles Sorce <m.sorce@gmail.com>
2017-08-17 16:03:42 -0700

    (feat) Set fixed dimensions for photo and text areas above schedule.

--- client/src/components/left/selection/index.js
+++ client/src/components/left/selection/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
 
 class Selection extends React.Component {
@@ -82,11 +83,26 @@ class Selection extends React.Component {
         flexWrap: 'nowrap',
         justifyContent: 'space-around',
       }}>
-        <div>
-          <img
-            style={{maxWidth: '100%'}}
-            src={this.props.selection.fullData.images[0].hostedLargeUrl}
-          />
+        <div style={{
+          boxSizing: 'border-box',
+          height: '150px'
+        }}>
+          <div 
+            cols="1" 
+            rows="1"
+            style={{
+              position: 'relative',
+              display: 'block'
+          }}> 
+            <img 
+              style={{
+                position: 'relative',
+                display: 'block',
+                height: '100%'
+              }}
+              src={this.props.selection.fullData.images[0].hostedLargeUrl}
+            />
+          </div>
         </div>
 
         <div style={{
@@ -94,16 +110,28 @@ class Selection extends React.Component {
           flexDirection: 'column',
           flexWrap: 'nowrap',
           justifyContent: 'space-between',
+          height: '145px'
         }}>
           <span style={{
             textAlign: 'center',
-            padding: '10px 0 10px 0',
+            padding: '6px 6px 6px 6px',
             backgroundColor: '#E0F2F1',
+            fontWeight: 'bold',
+            fontSize: '115%'
           }}>
             {this.props.selection.fullData.name}
           </span>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          <FlatButton label="Read More" onTouchTap={this.viewMoreHandler}/>
+          <p style={{
+            padding: '0px 5px 0px 10px',
+            height: '50px',
+            overflowY: 'scroll'
+          }}>{this.props.selection.abridgedData.ingredients.join(', ')}</p>
+          <RaisedButton 
+            label="Read More" 
+            onTouchTap={this.viewMoreHandler}
+            style={{
+              backgroundColor: '#E0F2F1'
+            }}/>
         </div>
         { dialogBox }
       </div> :
