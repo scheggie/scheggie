33d3fd84b6b73664f8607ee6e9e7e9dc628920a1
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-08-25 14:36:15 -0700

    use material UI for styling filter component

--- client/src/components/right/filter/panel.jsx
+++ client/src/components/right/filter/panel.jsx
@@ -1,56 +1,90 @@
 import React from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const styles = {
+  customWidth: {
+    width: 155,
+  },
+};
 
 class Panel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: ''
+      selected: this.props.default
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(event) {
-    console.log('the value clicked is ', event.target.value)
-    //var selected = ''
-    if (event.target.value.includes('Select')) {
+  handleClick(event, index, value) {
+    console.log('the event value clicked is ', typeof value)
+    console.log('the clicked value is ', value)
+    if (value.includes('Any')) {
       this.setState({
         selected: this.props.default
       }, function() {
-        this.props.updateCategoryTerm(this.props.category, this.state.selected);
+        if (this.props.category === 'cuisine') {
+          this.props.updateCategoryTerm(this.props.category, this.state.selected);
+        } else {
+          this.props.updateCategoryTerm(this.props.category, '10000');
+        }
+
       })
-      //selected = this.props.default
     } else {
         this.setState({
-          selected:event.target.value
+          selected:value
         }, function() {
           this.props.updateCategoryTerm(this.props.category, this.state.selected);
         })
-      //selected = event.target.value
     }
-
   }
 
   render() {
     if (this.props.category === 'totalTimeInSeconds') {
       return (
-        <div>
-          <select onChange = {this.handleClick}>
-            <option>Select {this.props.selectCategory}</option>
+        <div style = {{'display': 'inline-block', 'float': 'left', 'padding' :'25px'}}>
+          <SelectField
+            floatingLabelText = {this.props.selectCategory}
+            value = {this.state.selected}
+            onChange = {this.handleClick}
+            style={styles.customWidth}
+          >
+            <MenuItem
+              value = "Any"
+              primaryText = "Any"
+            />
             {this.props.choices.map((choice, index) =>
-              <option value = {choice} key = {index}>{choice/60} Min</option>
+              <MenuItem
+                value = {choice}
+                key = {index}
+                primaryText = {choice/60}
+              />
             )}
-          </select>
+          </SelectField>
         </div>
       )
     } else {
       return (
-        <div>
-          <select onChange = {this.handleClick}>
-            <option>Select {this.props.selectCategory}</option>
+        <div style = {{'display': 'inline-block', 'float': 'left', 'padding' :'25px'}}>
+          <SelectField
+            floatingLabelText = {this.props.selectCategory}
+            value = {this.state.selected}
+            onChange = {this.handleClick}
+            style={styles.customWidth}
+          >
+            <MenuItem
+              value = "Any"
+              primaryText = "Any"
+            />
             {this.props.choices.map((choice, index) =>
-              <option value = {choice} key = {index}>{choice}</option>
+              <MenuItem
+                value = {choice}
+                key = {index}
+                primaryText = {choice}
+              />
             )}
-          </select>
+          </SelectField>
         </div>
       )
     }
